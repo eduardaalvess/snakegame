c1cc0130d80f7e94f0bd682a099d1b4b379a8010
saintravi <Dudah4@gmail.com>
2021-05-15 09:16:12 -0300

    impedir q a cobrinha suma

--- script.js
+++ script.js
@@ -24,7 +24,23 @@ function criarCobrinha(){
 
 }
 
+document.addEventListener('keydown', update);
+
+function update (event){
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'down') direction = 'up';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'up') direction = 'down';
+}
+
 function iniciarJogo() {
+
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+
+
     criarBG();
     criarCobrinha();
 
@@ -32,8 +48,8 @@ function iniciarJogo() {
     let snakeY = snake[0].y;
 
     if(direction == "right") snakeX += box;
-    if(direction == "left") snakeX += box;
-    if(direction == "up") snakeY += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
     snake.pop();
